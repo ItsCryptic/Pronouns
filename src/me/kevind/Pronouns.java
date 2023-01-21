56d93bed6ed46d15cc492c8963d59fd23d5a3d82
Kevin D <kevin.diangelis@gmail.com>
2023-01-20 20:18:35 -0500

    commit again because why not

--- src/me/kevind/Pronouns.java
+++ src/me/kevind/Pronouns.java
@@ -1,6 +1,11 @@
 package me.kevind;
 
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class Pronouns extends JavaPlugin {
     private static Pronouns instance;
@@ -10,6 +15,15 @@ public class Pronouns extends JavaPlugin {
     @Override
     public void onEnable() {
         instance = this;
+        PluginManager manager = getServer().getPluginManager();
+        if (!(manager.isPluginEnabled("KevinUtils"))) {
+            getLogger().warning("Please install KevinUtils to use this plugin!");
+            manager.disablePlugin(this);
+        }else {
+            getLogger().info("Using KevinUtils");
+        }
+        getConfig().options().copyDefaults(true);
+        saveConfig();
     }
     public void onDisable() {
 
--- src/me/kevind/commands/PronounsCommand.java
+++ src/me/kevind/commands/PronounsCommand.java
@@ -1,16 +1,42 @@
 package me.kevind.commands;
 
+import me.kevind.Pronouns;
+import me.kevind.utils.MessageUtils;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class PronounsCommand implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if (sender instanceof Player) {
             Player player = (Player) sender;
+            if (args.length == 0) {
+                MessageUtils.sendMessage(player, "&cPlease ");
+            }
+            if (args[0].equalsIgnoreCase("set")) {
+                if (args[1] != null) {
+                    File file = new File(Pronouns.getInstance().getDataFolder(), "pronouns.yml");
+                    String search = args[1].toString();
+                    try(Scanner scanner = new Scanner(file)) {
+                        while (scanner.hasNextLine()) {
+                            String line = scanner.nextLine();
+                            if (line.contains(search)) {
+                                Pronouns.getInstance().getLogger().info("Found " + search);
+                            }
+                        }
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
         }
         return false;
     }
